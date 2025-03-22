6d263e7f7df2e328797c9fc97fe08852cecf9a4e
Muhammad Ahmad <maliktouseef525@gmail.com>
2025-03-23 02:09:05 +0500

    searchbox/done

--- lib/drawer/search.dart
+++ lib/drawer/search.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
 class SearchBox extends StatelessWidget {
-  const SearchBox({super.key});
-
+   SearchBox({super.key, required this.onChanged, required this.controller});
+final SearchController controller;
+final ValueChanged<String> onChanged;
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: 350,
-      height: 50,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 227, 227, 227),
-        borderRadius: BorderRadius.circular(30),
-        
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Search Shoes"),
-            Icon(Icons.search)
-          ],
-        ),
-      ),
+    return  TextField(
+controller: controller,
+onChanged: onChanged,
+decoration: InputDecoration(
+  hintText: "Shoes search",
+  prefixIcon: Icon(Icons.search),
+  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+),
     );
+    //  Container(
+    //   width: 350,
+    //   height: 50,
+    //   decoration: BoxDecoration(
+    //     color: const Color.fromARGB(255, 227, 227, 227),
+    //     borderRadius: BorderRadius.circular(30),
+        
+    //   ),
+    //   child: Padding(
+    //     padding: const EdgeInsets.all(12.0),
+    //     child: Row(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       children: [
+    //         Text("Search Shoes"),
+    //         Icon(Icons.search)
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
